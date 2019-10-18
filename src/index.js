99cb82a31ddf177e294cffbcecd7a036317dc468
Sungjun <hongsi140626@gmail.com>
2019-10-17 22:49:49 -0400

    Installed node-sass module

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; // In order to use Bootstrap
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './store/modules/index';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk'; // Redux Middleware 'Redux Thunk' (In order to use async)
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />    
+</Provider>, document.getElementById('root'));
