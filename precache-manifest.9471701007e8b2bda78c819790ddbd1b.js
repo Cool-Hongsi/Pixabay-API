033be8a71bc1a344d6ef9bbc688e164c080478f1
Sungjun <hongsi140626@gmail.com>
2019-10-23 03:44:33 -0400

    Updates

---
+++ precache-manifest.9471701007e8b2bda78c819790ddbd1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9fa5455a13e776a148e9648332e761",
+    "url": "/Pixabay-API/index.html"
+  },
+  {
+    "revision": "91ce1f17399ecfa4fafd",
+    "url": "/Pixabay-API/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6f9f7cd7bdccf79205e9",
+    "url": "/Pixabay-API/static/css/main.f2d9be39.chunk.css"
+  },
+  {
+    "revision": "91ce1f17399ecfa4fafd",
+    "url": "/Pixabay-API/static/js/2.5425e6ef.chunk.js"
+  },
+  {
+    "revision": "6f9f7cd7bdccf79205e9",
+    "url": "/Pixabay-API/static/js/main.5ec695c8.chunk.js"
+  },
+  {
+    "revision": "bc67145eea2eedcc1512",
+    "url": "/Pixabay-API/static/js/runtime-main.a9bbb106.js"
+  }
+]);
